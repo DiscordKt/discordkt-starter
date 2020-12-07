b4b3ac2751402c4c8fc1165d056c89a0da19b51d
Jake Mattson <JakeJMattson@gmail.com>
2020-12-07 11:19:15 -0600

    Build script updates

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,11 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
 group = "me.ddivad"
 version = Versions.BOT
 description = "discordKt starter template"
 
 plugins {
-    kotlin("jvm") version "1.4.10"
-    id("com.github.johnrengelman.shadow") version "6.0.0"
+    kotlin("jvm") version "1.4.21"
+    id("com.github.johnrengelman.shadow") version "6.1.0"
+    id("com.github.ben-manes.versions") version "0.36.0"
 }
 
 repositories {
@@ -19,7 +18,7 @@ dependencies {
 }
 
 tasks {
-    withType<KotlinCompile> {
+    compileKotlin {
         kotlinOptions.jvmTarget = "1.8"
     }
 
@@ -27,12 +26,11 @@ tasks {
         archiveFileName.set("Starter.jar")
         manifest {
             attributes(
-                    "Main-Class" to "me.ddivad.starter.MainKt"
+                "Main-Class" to "me.ddivad.starter.MainKt"
             )
         }
     }
 }
-
 
 object Versions {
     const val BOT = "1.0.0"
